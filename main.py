d09e903f2f538017e0f881b7dadb7634c225b096
Vinayak M <vinayakmahajan06@gmail.com>
2023-11-10 23:53:35 +0530

    updated

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-'''	
-""Not a final product""
-
-'''
-
-
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import datetime
 import wikipedia
-import pyjokes
+
+# import os
+# import openai
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# completion = openai.ChatCompletion.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+#     {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+#   ]
+# )
+
+# print(completion.choices[0].message)
+
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
